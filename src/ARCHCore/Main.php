d6c4e66e82cb22d5a25bfe51f3d40d6217f890f3
MyrulXzavier (GamerXzavier) <GamerXzavier@gmail.com>
2016-07-02 21:08:23 +0800

    Initials Commits

--- src/ARCHCore/Main.php
+++ src/ARCHCore/Main.php
@@ -38,6 +38,7 @@ use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\block\BlockPlaceEvent;
 use pocketmine\event\player\PlayerRespawnEvent;
 use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\event\player\PlayerCommandPreprocessEvent;
 use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\event\player\PlayerPreLoginEvent;
@@ -704,6 +705,254 @@ public function OpGoldenApple(PlayerItemConsumeEvent $event){
 			$player->addEffect(Effect::getEffect(8)->setDuration($distance * 2)->setAmplifier(255)->setAmbient(false)->setVisible(true));
 		}
 	}
+/*Plugin AddStrikex*/
+    public function addStrikex(Player $p, $height){
+
+$level = $p->getLevel();
+
+$light = new AddEntityPacket();
+
+$light->type = 93;
+
+$light->eid = Entity::$entityCount++;
+
+$light->metadata = array();
+
+$light->speedX = 0;
+
+$light->speedY = 0;
+
+$light->speedZ = 0;
+
+$light->yaw = $p->getYaw();
+
+$light->pitch = $p->getPitch();
+
+$light->x = $p->x;
+
+$light->y = $p->y+$height;
+
+$light->z = $p->z;
+
+Server::broadcastPacket($level->getPlayers(),$light);
+
+   }
+/*Plugin OnJoinxez*/
+    public function onJoinxez(PlayerJoinEvent $e){
+	$p = $e->getPlayer();
+	$light = new AddEntityPacket();
+        $light->type = 93;
+        $light->eid = Entity::$entityCount++;
+        $light->metadata = array();
+        $light->speedX = 0;
+        $light->speedY = 0;
+        $light->speedZ = 0;
+        $light->yaw = $p->getYaw();
+        $light->pitch = $p->getPitch();
+        $light->x = $p->x;
+        $light->y = $p->y;
+        $light->z = $p->z;
+        $p->dataPacket($light);
+    	
+    }
+/*Plugin OnQuitxez*/
+    public function onQuitxez(PlayerQuitEvent $e){
+	$p = $e->getPlayer();
+	$light = new AddEntityPacket();
+        $light->type = 93;
+        $light->eid = Entity::$entityCount++;
+        $light->metadata = array();
+        $light->speedX = 0;
+        $light->speedY = 0;
+        $light->speedZ = 0;
+        $light->yaw = $p->getYaw();
+        $light->pitch = $p->getPitch();
+        $light->x = $p->x;
+        $light->y = $p->y;
+        $light->z = $p->z;
+        $p->dataPacket($light);
+    	
+    }
+/*Plugin OnDamagexzx*/
+    public function onDamagexzx(EntityDamageEvent $e){
+        /** @var Player $p */
+        $p = $e->getEntity();
+        if (!$p instanceof Player){
+            return;
+        }
+
+        if ($e instanceof EntityDamageByEntityEvent){
+            /** @var Player $pl */
+            $pl = $e->getDamager();
+            if (!$pl instanceof Player){
+                return;
+            }
+            $it = $pl->getInventory()->getItemInHand();
+            if (!$it->hasEnchantments()){
+                return;
+            }
+            $en = $it->getEnchantments();
+            foreach ($en as $ench){
+                $lvl = $ench->getLevel();
+                switch ($ench->getId()){
+                    case 9:
+                        $e->setDamage($e->getDamage()+($lvl*1.25));
+                        break;
+                    case 12:
+                        $e->setKnockback($e->getKnockBack()+($lvl*0.3));
+                        break;
+                    case 13:
+                        if (!$e->isCancelled()){
+                            $p->setOnFire($lvl*4);
+                        }
+                        break;
+                    case 19:
+                        $dmg = \round((($lvl+1)/4));
+                        $e->setDamage($e->getDamage()+$dmg);
+                        break;
+                    case 20:
+                        $e->setKnockBack($e->getKnockBack()+($lvl*0.4));
+                        break;
+                    case 21:
+                        if (!$e->isCancelled()){
+                            $p->setOnFire(5);
+                        }
+                        break;
+                    case 22:
+                        $pl->getInventory()->addItem(Item::ARROW, 0, 1);
+                        break;
+                }
+            }
+        }
+
+        foreach ($p->getInventory()->getArmorContents() as $item){
+            $eng = $item->getEnchantments();
+            foreach ($eng as $enchantment){
+                $lvl = $enchantment->getLevel();
+                switch ($enchantment->getId()){
+                    case 0:
+                        $e->setDamage($e->getDamage() - (($lvl*0.04)*$e->getDamage()));
+                        break;
+                    case 1:
+                        if ($e->getCause() > 4 && $e->getCause() < 8){
+                            $e->setDamage($e->getDamage() - (($lvl*0.12)*$e->getDamage()));
+                        }
+                        break;
+                    case 2:
+                        if ($e->getCause() == 4){
+                            $e->setDamage($e->getDamage() - (($lvl*0.15)*$e->getDamage()));
+                        }
+                        break;
+                    case 3:
+                        if ($e->getCause() > 8 && $e->getCause() < 11){
+                            $e->setDamage($e->getDamage() - (($lvl*0.15)*$e->getDamage()));
+                        }
+                        break;
+                    case 4:
+                        if ($e->getCause() == 2){
+                            $e->setDamage($e->getDamage() - (($lvl*0.12)*$e->getDamage()));
+                        }
+                        break;
+                    case 7:
+                        if ($e instanceof EntityDamageByEntityEvent){
+                            /** @var Player $pl */
+                            $pl = $e->getDamager();
+                            Server::getInstance()->getPluginManager()->callEvent($ev = new EntityDamageEvent($pl, 14, $lvl*2));
+                            if ($ev->isCancelled() || $ev->getDamage() <= 0){
+                                break;
+                            }
+                            $pl->attack($lvl*2, $ev);
+                        }
+                        break;
+                }
+            }
+        }
+    }
+/*Plugin OnBreakszz*/
+    public function onBreakszz(BlockBreakEvent $e){
+        $p = $e->getPlayer();
+        if (!$p->getInventory()->getItemInHand()->hasEnchantments()){
+            return;
+        }
+        $ench = $p->getInventory()->getItemInHand()->getEnchantments();
+        foreach ($ench as $en){
+            $lvl = $en->getLevel();
+            switch ($en->getId()){
+                case 16:
+                    $item = [$e->getBlock()];
+                    $e->setDrops($item);
+                    break;
+                case 17:
+                    if (\mt_rand(1, (6-$lvl)) === 2){
+                        $i = $p->getInventory()->getItemInHand();
+                        $i->setDamage($i->getDamage()+1);
+                    }
+                    break;
+                case 18:
+                    switch ($e->getBlock()->getId()){
+                        case 16:
+                            $drop = \mt_rand(3, 3+$lvl);
+                            $e->setDrops([Item::get(263, 0, $drop)]);
+                            break;
+                        case 21:
+                            $drop = \mt_rand(5, 5+$lvl);
+                            $e->setDrops([Item::get(351, 4, $drop)]);
+                            break;
+                        case 56:
+                            $drop = \mt_rand(1, 1+$lvl);
+                            $e->setDrops([Item::get(264, 0, $drop)]);
+                            break;
+                        case 73:
+                            $drop = \mt_rand(5, 5+$lvl);
+                            $e->setDrops([Item::get(331, 0, $drop)]);
+                            break;
+                        case 89:
+                            $e->setDrops([Item::get(16, 0, 4)]);
+                            break;
+                        case 129:
+                            $drop = \mt_rand(1, \round(1+($lvl/3)));
+                            $e->setDrops([Item::get(129, 0, $drop)]);
+                            break;
+                        case 153:
+                            $drop = \mt_rand(2, 2+$lvl);
+                            $e->setDrops([Item::get(406, 0, $drop)]);
+                            break;
+                    }
+                    break;
+            }
+        }
+    }	 
+/*Plugin TranslateColors*/
+	public function translateColors($symbol, $message){
+		
+		$message = str_replace($symbol."0", TextFormat::BLACK, $message);
+		$message = str_replace($symbol."1", TextFormat::DARK_BLUE, $message);
+		$message = str_replace($symbol."2", TextFormat::DARK_GREEN, $message);
+		$message = str_replace($symbol."3", TextFormat::DARK_AQUA, $message);
+		$message = str_replace($symbol."4", TextFormat::DARK_RED, $message);
+		$message = str_replace($symbol."5", TextFormat::DARK_PURPLE, $message);
+		$message = str_replace($symbol."6", TextFormat::GOLD, $message);
+		$message = str_replace($symbol."7", TextFormat::GRAY, $message);
+		$message = str_replace($symbol."8", TextFormat::DARK_GRAY, $message);
+		$message = str_replace($symbol."9", TextFormat::BLUE, $message);
+		$message = str_replace($symbol."a", TextFormat::GREEN, $message);
+		$message = str_replace($symbol."b", TextFormat::AQUA, $message);
+		$message = str_replace($symbol."c", TextFormat::RED, $message);
+		$message = str_replace($symbol."d", TextFormat::LIGHT_PURPLE, $message);
+		$message = str_replace($symbol."e", TextFormat::YELLOW, $message);
+		$message = str_replace($symbol."f", TextFormat::WHITE, $message);
+		
+		$message = str_replace($symbol."k", TextFormat::OBFUSCATED, $message);
+		$message = str_replace($symbol."l", TextFormat::BOLD, $message);
+		$message = str_replace($symbol."m", TextFormat::STRIKETHROUGH, $message);
+		$message = str_replace($symbol."n", TextFormat::UNDERLINE, $message);
+		$message = str_replace($symbol."o", TextFormat::ITALIC, $message);
+		$message = str_replace($symbol."r", TextFormat::RESET, $message);
+		
+		return $message;
+	}
+	
+
 
 /*/
  *  More Features Coming Soon.
