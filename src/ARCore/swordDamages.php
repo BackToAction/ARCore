90f778de5997f40b242993abd4d050e0364e7108
MyrulXzavier (GamerXzavier) <GamerXzavier@gmail.com>
2016-08-02 21:01:22 +0800

    Create swordDamages.php

---
+++ src/ARCore/swordDamages.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ARCore;
+
+class swordDamages{
+
+    const DAMAGE_VALUES = array(
+        "SWORD" => 5,
+        "AXE" => 4,
+        "PICKAXE" => 3,
+        "SHOVEL" => 2,
+        "OTHER" => 1
+    );
+    const MATERIAL_VALUES = array(
+        "WOOD" => 0,
+        "GOLD" => 0,
+        "STONE" => 1,
+        "IRON" => 2,
+        "DIAMOND" => 3
+    );
+}
