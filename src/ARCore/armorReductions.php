676efd874a33f60c290ad1c65533162f78be3abe
MyrulXzavier (GamerXzavier) <GamerXzavier@gmail.com>
2016-08-02 21:00:13 +0800

    Create armorReductions.php

---
+++ src/ARCore/armorReductions.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace ARCore;
+
+class armorReductions{
+
+    const MATERIAL_VALUES_HEAD = array(
+        "NONE" => 0,
+        "LEATHER" => 1,
+        "GOLD" => 2,
+        "CHAIN" => 2,
+        "IRON" => 2,
+        "DIAMOND" => 3
+    );
+    const MATERIAL_VALUES_CHEST = array(
+        "NONE" => 0,
+        "LEATHER" => 3,
+        "GOLD" => 5,
+        "CHAIN" => 5,
+        "IRON" => 6,
+        "DIAMOND" => 8
+    );
+    const MATERIAL_VALUES_LEGGINGS = array(
+        "NONE" => 0,
+        "LEATHER" => 2,
+        "GOLD" => 3,
+        "CHAIN" => 4,
+        "IRON" => 5,
+        "DIAMOND" => 6
+    );
+    const MATERIAL_VALUES_BOOTS = array(
+        "NONE" => 0,
+        "LEATHER" => 1,
+        "GOLD" => 1,
+        "CHAIN" => 1,
+        "IRON" => 2,
+        "DIAMOND" => 3
+    );
+}
