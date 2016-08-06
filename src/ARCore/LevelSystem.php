ab8fdc9b516ba77b7064c602aec7ba848a876cb4
MyrulXzavier (GamerXzavier) <GamerXzavier@gmail.com>
2016-08-06 20:59:06 +0800

    First commit for updating LevelSystem.php
    I'm writing using phone cause PC a bit slow xP

--- src/ARCore/LevelSystem.php
+++ src/ARCore/LevelSystem.php
@@ -1,5 +1,26 @@
 <?php
+//might rename this namespace
+namespace ARCore;
+//between I'm writing this using phone so might mess up and if anyone see this message when to update this make sure to delete this comment.
+use pocketmine\player;
+use pocketmine\server;
+use pocketmine\plugin\PluginBase;
+use pocketmine\level\Position;
+use pocketmine\level\Location;
+use pocketmine\level\Position\getLevel;
+use pocketmine\level\format\Chunk;
+use pocketmine\level\format\FullChunk;
+use pocketmine\level\Level;
+use pocketmine\level\particle\FloatingTextParticle;
+use pocketmine\math\Vector3;
+use pocketmine\math\Math;
+use pocketmine\math\AxisAlignedBB;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\scheduler\CallbackTask;
 
-namespace ARCore
+//P.S if you checking for exp find it on Pocketmine/Player.php
+class LevelSystem extends PluginBase implements Listener{
 
-*/TODO/*
+/*TODO*/
+
+}
