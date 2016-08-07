3f040e6840824d2b33e07d040317b6fd94f04409
Adam Le <adamsuper169@gmail.com>
2016-08-07 09:40:31 +0700

    Check if I right or wrong.

--- src/ARCore/M4K/M4K.php
+++ src/ARCore/M4K/M4K.php
@@ -16,51 +16,11 @@ use pocketmine\event\player\PlayerDeathEvent;
 use onebone\economyapi\EconomyAPI;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\entity\EntityDamageEvent;
+use ARCore\ARCore;
 
-	class Main extends PluginBase implements Listener
-	
-	{
-		public function onEnable()
-		{
-			$this->getServer()->getPluginManager()->registerEvents($this,$this);
-			$this->getLogger()->info(MT::AQUA."M4K Registered!");
-			$this->api = EconomyAPI::getInstance();
-			
-			if (!file_exists($this->getDataFolder()))
-			{
-				@mkdir($this->getDataFolder(), true);
-			}
-			$this->config = new Config($this->getDataFolder(). "config.yml", Config::YAML, array("LMOAKiller" => 10,"LMOALoser"=> 10));	
-		}
-	
-		public function onPlayerDeathEvent(PlayerDeathEvent $event)
-		{
-			$player = $event->getEntity();
-			$name = strtolower($player->getName());
-		
-			if ($player instanceof Player)
-			{
-				$cause = $player->getLastDamageCause();
-		
-				if($cause instanceof EntityDamageByEntityEvent)
-				{
-					$damager = $cause->getDamager();
-					
-					if($damager instanceof Player)
-					{
-						$LMOAKiller = $this->config->get("LMOAKiller");
-						$LMOALoser = $this->config->get("LMOALoser");
-						$damager->sendPopup(MT::GOLD."You get $LMOAKiller Coins For Killing!");
-						$player->sendPopup(MT::GOLD."You lose $LMOALoser Coins For Dying!");
-						$this->api->addMoney($damager, $LMOAKiller);
-						$this->api->reduceMoney($player, $LMOALoser);
-					}
-				}
-			}
-		}
-		
-		public function onDisable()
-		{
-			$this->getLogger()->info(MT::AQUA."Plugin unloaded!");
-		}
+class M4K extends PluginBase implements Listener{
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		//TODO
 	}
+}
