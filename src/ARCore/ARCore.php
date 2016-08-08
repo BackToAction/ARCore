22d4e27c389b0695edd155d212b1711746d4a479
MyrulXzavier (GamerXzavier) <GamerXzavier@gmail.com>
2016-08-08 19:21:30 +0800

    you should do this xP
    between neurobinds project have effects commands

--- src/ARCore/ARCore.php
+++ src/ARCore/ARCore.php
@@ -105,10 +105,25 @@ use pocketmine\nbt\tag\ListTag;
 use pocketmine\nbt\tag\CompoundTag;
 //permission
 use pocketmine\permission\Permission;
+//trying to add clans
+use ARCore\Clans\FactionCommands;
+use ARCore\Clans\FactionListener;
 
 class ARCore extends PluginBase implements Listener{
+	
+	public $db;
+	public $prefs;
+
 /*Plugins OnEnable*/
-   public function onEnable(){	
+   public function onEnable(){		
+//implementing clans!	
+		@mkdir($this->getDataFolder());
+		
+		if(!file_exists($this->getDataFolder() . "BannedNames.txt")) {
+			$file = fopen($this->getDataFolder() . "BannedNames.txt", "w");
+			$txt = "Admin:admin:Staff:staff:Owner:owner:Builder:builder:Op:OP:op";
+			fwrite($file, $txt);
+		}
 //load level/world
         $this->getServer()->loadLevel("ArchLobby");
 /*Server Name*/
@@ -119,6 +134,42 @@ class ARCore extends PluginBase implements Listener{
        $this->getLogger()->info("Loading...");
        $this->getLogger()->info("Enabling...");
        $this->getLogger()->info("Enabled...");
+	//trying to implement clans!
+		$this->getServer()->getPluginManager()->registerEvents(new FactionListener($this), $this);
+		$this->fCommand = new FactionCommands($this);
+		
+		$this->prefs = new Config($this->getDataFolder() . "Prefs.yml", CONFIG::YAML, array(
+		"CreateCost" => 3000,
+		"ClaimCost" => 100000,
+		"OverClaimCost" => 25000,
+		"AllyCost" => 5000,
+		"AllyPrice" => 5000,
+		"SetHomeCost" => 150,
+		"MaxFactionNameLength" => 4,
+		"MaxPlayersPerFaction" => 100,
+		"OnlyLeadersAndOfficersCanInvite" => true,
+		"OfficersCanClaim" => false,
+		"PlotSize" => 30,
+                "PlayersNeededInFactionToClaimAPlot" => 5,
+                "PowerNeededToClaimAPlot" => 1000,
+                "PowerNeededToSetOrUpdateAHome" => 250,
+                "PowerGainedPerPlayerInFaction" => 50,
+                "PowerGainedPerKillingAnEnemy" => 15, 
+		"PowerReducedPerDeathByAnEnemy" => 10,
+                "PowerGainedPerAlly" => 100,
+                "TheDefaultPowerEveryFactionStartsWith" => 0,
+                "EnableOverClaim" => true,
+		));
+		$this->db = new \SQLite3($this->getDataFolder() . "FactionPower.db");
+		$this->db->exec("CREATE TABLE IF NOT EXISTS master (player TEXT PRIMARY KEY COLLATE NOCASE, faction TEXT, rank TEXT);");
+		$this->db->exec("CREATE TABLE IF NOT EXISTS confirm (player TEXT PRIMARY KEY COLLATE NOCASE, faction TEXT, invitedby TEXT, timestamp INT);");
+		$this->db->exec("CREATE TABLE IF NOT EXISTS alliance (player TEXT PRIMARY KEY COLLATE NOCASE, faction TEXT, requestedby TEXT, timestamp INT);");
+		$this->db->exec("CREATE TABLE IF NOT EXISTS motdrcv (player TEXT PRIMARY KEY, timestamp INT);");
+		$this->db->exec("CREATE TABLE IF NOT EXISTS motd (faction TEXT PRIMARY KEY, message TEXT);");
+		$this->db->exec("CREATE TABLE IF NOT EXISTS plots(faction TEXT PRIMARY KEY, x1 INT, z1 INT, x2 INT, z2 INT);");
+		$this->db->exec("CREATE TABLE IF NOT EXISTS home(faction TEXT PRIMARY KEY, x INT, y INT, z INT);");
+		$this->db->exec("CREATE TABLE IF NOT EXISTS strength(faction TEXT PRIMARY KEY, power INT);");
+		$this->db->exec("CREATE TABLE IF NOT EXISTS allies(ID INT PRIMARY KEY,faction1 TEXT, faction2 TEXT);");
 			}
 
    
@@ -126,7 +177,9 @@ class ARCore extends PluginBase implements Listener{
    public function onDisable(){
        $this->getLogger()->info("Loading...");
        $this->getLogger()->info("Disabling...");
-       $this->getLogger()->info("Disabled...");
+       $this->getLogger()->info("Disabled...");    
+		$this->db->close();
+	
    }
 /*Plugin OnJoin*/
    public function onJoin(PlayerJoinEvent $event){ 
@@ -468,6 +521,216 @@ class ARCore extends PluginBase implements Listener{
         }
         return "NONE";
     }
+//trying to implement clans!
+
+		
+	public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
+		$this->fCommand->onCommand($sender, $command, $label, $args);
+	}
+	public function isInFaction($player) {
+		$player = strtolower($player);
+		$result = $this->db->query("SELECT * FROM master WHERE player='$player';");
+		$array = $result->fetchArray(SQLITE3_ASSOC);
+		return empty($array) == false;
+	}
+    public function setFactionPower($faction,$power){ 
+		$stmt = $this->db->prepare("INSERT OR REPLACE INTO strength (faction, power) VALUES (:faction, :power);");   
+        $stmt->bindValue(":faction", $faction);
+		$stmt->bindValue(":power", $power);
+		$result = $stmt->execute();
+    }
+    public function setAllies($faction1, $faction2){
+        $stmt = $this->db->prepare("INSERT INTO allies (faction1, faction2) VALUES (:faction1, :faction2);");  
+        $stmt->bindValue(":faction1", $faction1);
+		$stmt->bindValue(":faction2", $faction2);
+		$result = $stmt->execute();
+    }
+    public function areAllies($faction1, $faction2){
+        $result = $this->db->query("SELECT * FROM allies WHERE faction1 = '$faction1' AND faction2 = '$faction2';");
+        $resultArr = $result->fetchArray(SQLITE3_ASSOC);
+        if(empty($resultArr)==false){
+            return true;
+        } 
+    } 
+    public function deleteAllies($faction1, $faction2){
+        $stmt = $this->db->prepare("DELETE FROM allies WHERE faction1 = '$faction1' AND faction2 = '$faction2';");   
+		$result = $stmt->execute();
+    }
+    public function getFactionPower($faction){
+        $result = $this->db->query("SELECT * FROM strength WHERE faction = '$faction';");
+        $resultArr = $result->fetchArray(SQLITE3_ASSOC);
+        return (int) $resultArr["power"];
+    }
+    public function addFactionPower($faction, $power){
+		$stmt = $this->db->prepare("INSERT OR REPLACE INTO strength (faction, power) VALUES (:faction, :power);");   
+        $stmt->bindValue(":faction", $faction);
+		$stmt->bindValue(":power", $this->getFactionPower($faction) + $power);
+		$result = $stmt->execute();
+    }
+    public function subtractFactionPower($faction,$power){
+		$stmt = $this->db->prepare("INSERT OR REPLACE INTO strength (faction, power) VALUES (:faction, :power);");   
+        $stmt->bindValue(":faction", $faction);
+		$stmt->bindValue(":power", $this->getFactionPower($faction) - $power);
+		$result = $stmt->execute();
+    }
+        
+	public function isLeader($player) {
+		$faction = $this->db->query("SELECT * FROM master WHERE player='$player';");
+		$factionArray = $faction->fetchArray(SQLITE3_ASSOC);
+		return $factionArray["rank"] == "Leader";
+	}
+	
+	public function isOfficer($player) {
+		$faction = $this->db->query("SELECT * FROM master WHERE player='$player';");
+		$factionArray = $faction->fetchArray(SQLITE3_ASSOC);
+		return $factionArray["rank"] == "Officer";
+	}
+	
+	public function isMember($player) {
+		$faction = $this->db->query("SELECT * FROM master WHERE player='$player';");
+		$factionArray = $faction->fetchArray(SQLITE3_ASSOC);
+		return $factionArray["rank"] == "Member";
+	}
+	
+	public function getPlayerFaction($player) {
+		$faction = $this->db->query("SELECT * FROM master WHERE player='$player';");
+		$factionArray = $faction->fetchArray(SQLITE3_ASSOC);
+		return $factionArray["faction"];
+	}
+	
+	public function getLeader($faction) {
+		$leader = $this->db->query("SELECT * FROM master WHERE faction='$faction' AND rank='Leader';");
+		$leaderArray = $leader->fetchArray(SQLITE3_ASSOC);
+		return $leaderArray['player'];
+	}
+	
+	public function factionExists($faction) {
+		$result = $this->db->query("SELECT * FROM master WHERE faction='$faction';");
+		$array = $result->fetchArray(SQLITE3_ASSOC);
+		return empty($array) == false;
+	}
+	
+	public function sameFaction($player1, $player2) {
+		$faction = $this->db->query("SELECT * FROM master WHERE player='$player1';");
+		$player1Faction = $faction->fetchArray(SQLITE3_ASSOC);
+		$faction = $this->db->query("SELECT * FROM master WHERE player='$player2';");
+		$player2Faction = $faction->fetchArray(SQLITE3_ASSOC);
+		return $player1Faction["faction"] == $player2Faction["faction"];
+	}
+	
+	public function getNumberOfPlayers($faction) {
+		$query = $this->db->query("SELECT COUNT(*) as count FROM master WHERE faction='$faction';");
+		$number = $query->fetchArray();
+		return $number['count'];
+	}
+	
+	public function isFactionFull($faction) {
+		return $this->getNumberOfPlayers($faction) >= $this->prefs->get("MaxPlayersPerFaction");
+        
+	}
+	
+	public function isNameBanned($name) {
+		$bannedNames = explode(":", file_get_contents($this->getDataFolder() . "BannedNames.txt"));
+		return in_array($name, $bannedNames);
+	}
+	
+    public function newPlot($faction, $x1, $z1, $x2, $z2) {
+		$stmt = $this->db->prepare("INSERT OR REPLACE INTO plots (faction, x1, z1, x2, z2) VALUES (:faction, :x1, :z1, :x2, :z2);");
+		$stmt->bindValue(":faction", $faction);
+		$stmt->bindValue(":x1", $x1);
+		$stmt->bindValue(":z1", $z1);
+		$stmt->bindValue(":x2", $x2);
+		$stmt->bindValue(":z2", $z2);
+		$result = $stmt->execute();
+	}
+	public function drawPlot($sender, $faction, $x, $y, $z, $level, $size) {
+		$arm = ($size - 1) / 2;
+		$block = new Snow();
+		if($this->cornerIsInPlot($x + $arm, $z + $arm, $x - $arm, $z - $arm)) {
+			$claimedBy = $this->factionFromPoint($x, $z);
+            $power_claimedBy = $this->getFactionPower($claimedBy);
+            $power_sender = $this->getFactionPower($faction);
+
+            if($this->prefs->get("EnableOverClaim")){
+                if($power_sender < $power_claimedBy){
+                    $sender->sendMessage($this->formatMessage("This area is aleady claimed by $claimedBy with power $power_claimedBy. Your Clan has $power_sender power. You can not overclaim this plot."));
+                } else {
+                    $sender->sendMessage($this->formatMessage("§6- §3This area is aleady claimed by §e$claimedBy §3with power §b$power_claimedBy §3. Your Clan has $power_sender power. Type /c overclaim to overclaim this plot if you want."));
+                }
+                return false;
+            } else {
+			    $sender->sendMessage($this->formatMessage("§6- §3This area is aleady claimed by $claimedBy with power $power_claimedBy. §cOverclaiming is disabled."));
+			    return false;
+            }
+		}
+		$level->setBlock(new Vector3($x + $arm, $y, $z + $arm), $block);
+		$level->setBlock(new Vector3($x - $arm, $y, $z - $arm), $block);
+		$this->newPlot($faction, $x + $arm, $z + $arm, $x - $arm, $z - $arm);
+		return true;
+	}
+	
+	public function isInPlot($player) {
+		$x = $player->getFloorX();
+		$z = $player->getFloorZ();
+		$result = $this->db->query("SELECT * FROM plots WHERE $x <= x1 AND $x >= x2 AND $z <= z1 AND $z >= z2;");
+		$array = $result->fetchArray(SQLITE3_ASSOC);
+		return empty($array) == false;
+	}
+   
+	
+	public function factionFromPoint($x,$z) {
+		$result = $this->db->query("SELECT * FROM plots WHERE $x <= x1 AND $x >= x2 AND $z <= z1 AND $z >= z2;");
+		$array = $result->fetchArray(SQLITE3_ASSOC);
+		return $array["faction"];
+	}
+   
+	
+	public function inOwnPlot($player) {
+		$playerName = $player->getName();
+		$x = $player->getFloorX();
+		$z = $player->getFloorZ();
+		return $this->getPlayerFaction($playerName) == $this->factionFromPoint($x, $z);
+	}
+	
+	public function pointIsInPlot($x,$z) {
+		$result = $this->db->query("SELECT * FROM plots WHERE $x <= x1 AND $x >= x2 AND $z <= z1 AND $z >= z2;");
+		$array = $result->fetchArray(SQLITE3_ASSOC);
+		return !empty($array);
+	}
+	
+	public function cornerIsInPlot($x1, $z1, $x2, $z2) {
+		return($this->pointIsInPlot($x1, $z1) || $this->pointIsInPlot($x1, $z2) || $this->pointIsInPlot($x2, $z1) || $this->pointIsInPlot($x2, $z2));
+	}
+	
+	public function formatMessage($string, $confirm = false) {
+		if($confirm) {
+			return "" . TextFormat::RED . "•RED Clan•" . TextFormat::WHITE . "•" . TextFormat::GREEN . "$string";
+		} else {	
+			return "" . TextFormat::RED . "•RED Clan•" . TextFormat::WHITE . "•" . TextFormat::RED . "$string";
+		}
+	}
+	
+	public function motdWaiting($player) {
+		$stmt = $this->db->query("SELECT * FROM motdrcv WHERE player='$player';");
+		$array = $stmt->fetchArray(SQLITE3_ASSOC);
+		$this->getServer()->getLogger()->info("\$player = " . $player);
+		return !empty($array);
+	}
+	
+	public function getMOTDTime($player) {
+		$stmt = $this->db->query("SELECT * FROM motdrcv WHERE player='$player';");
+		$array = $stmt->fetchArray(SQLITE3_ASSOC);
+		return $array['timestamp'];
+	}
+	
+	public function setMOTD($faction, $player, $msg) {
+		$stmt = $this->db->prepare("INSERT OR REPLACE INTO motd (faction, message) VALUES (:faction, :message);");
+		$stmt->bindValue(":faction", $faction);
+		$stmt->bindValue(":message", $msg);
+		$result = $stmt->execute();
+		
+		$this->db->query("DELETE FROM motdrcv WHERE player='$player';");
+	}
 
 
 
