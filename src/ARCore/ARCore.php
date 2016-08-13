b91a9754daf111563a7546569ba454c83c26641b
Adam Le <adamsuper169@gmail.com>
2016-08-13 20:13:26 +0700

    Check if I right or wrong.

--- src/ARCore/ARCore.php
+++ src/ARCore/ARCore.php
@@ -117,9 +117,134 @@ class ARCore extends PluginBase implements Listener{
 	
 	public $db;
 	public $prefs;
+	public static $pet;
+	public static $petState;
+	public $pettype;
+	public $price;
+	public $wishPet;
+	public static $isPetChanging;
+	public static $type;
 
 /*Plugins OnEnable*/
-   public function onEnable(){		
+   public function onEnable(){
+/*Pets Plugin*/
+	public function onEnable() {
+		@mkdir($this->getDataFolder());
+		@mkdir($this->getDataFolder() . "PetPlayer");
+		$server = Server::getInstance();
+		$server->getCommandMap()->register('pets', new PetCommand($this,"pets"));
+		Entity::registerEntity(OcelotPet::class);
+		Entity::registerEntity(WolfPet::class);
+		Entity::registerEntity(PigPet::class);
+		Entity::registerEntity(SheepPet::class);
+		Entity::registerEntity(RabbitPet::class);
+		Entity::registerEntity(ChickenPet::class);
+		Entity::registerEntity(BatPet::class);
+		Entity::registerEntity(MagmaPet::class);
+		Entity::registerEntity(SilverfishPet::class);
+		Entity::registerEntity(BlockPet::class);
+		$this->saveDefaultConfig();
+		$this->getServer()->getLogger()->info(TextFormat::BLUE . "Enable Pets for ARCore!");
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+	public function create($player,$type, Position $source, ...$args) {
+		$chunk = $source->getLevel()->getChunk($source->x >> 4, $source->z >> 4, true);
+		$nbt = new CompoundTag("", [
+			"Pos" => new ListTag("Pos", [
+				new DoubleTag("", $source->x),
+				new DoubleTag("", $source->y),
+				new DoubleTag("", $source->z)
+					]),
+			"Motion" => new ListTag("Motion", [
+				new DoubleTag("", 0),
+				new DoubleTag("", 0),
+				new DoubleTag("", 0)
+					]),
+			"Rotation" => new ListTag("Rotation", [
+				new FloatTag("", $source instanceof Location ? $source->yaw : 0),
+				new FloatTag("", $source instanceof Location ? $source->pitch : 0)
+					]),
+		]);
+		$pet = Entity::createEntity($type, $chunk, $nbt, ...$args);
+		$data = new Config($this->getDataFolder() . "players/" . strtolower($player->getName()) . ".yml", Config::YAML);
+		$data->set("type", $type); 
+		$data->save();
+		$pet->setOwner($player);
+		$pet->spawnToAll();
+		return $pet; 
+	}
+	public function createPet(Player $player, $type, $holdType = "") {
+ 		if (isset($this->pet[$player->getName()]) != true) {	
+			$len = rand(8, 12); 
+			$x = (-sin(deg2rad($player->yaw))) * $len  + $player->getX();
+			$z = cos(deg2rad($player->yaw)) * $len  + $player->getZ();
+			$y = $player->getLevel()->getHighestBlockAt($x, $z);
+			$source = new Position($x , $y + 2, $z, $player->getLevel());
+			if (isset(self::$type[$player->getName()])){
+				$type = self::$type[$player->getName()];
+			}
+ 			switch ($type){
+ 				case "WolfPet":
+ 				break;
+ 				case "RabbitPet":
+ 				break;
+ 				case "PigPet":
+ 				break;
+ 				case "SheepPet":
+ 				break;
+ 				case "OcelotPet":
+ 				break;
+ 				case "ChickenPet":
+ 				break;
+ 				case "BatPet":
+ 				break;
+ 				case "MagmaPet":
+ 				break;
+ 				case "SilverfishPet":
+ 				break;
+ 				case "BlockPet":
+ 				break;
+ 				default:
+ 					$pets = array("OcelotPet", "PigPet", "SheepPet", "WolfPet",  "RabbitPet", "ChickenPet", "BatPet", "MagmaPet", "BlockPet", "SilverfishPet");
+ 					$type = $pets[rand(0, 10)];
+ 			}
+			$pet = $this->create($player,$type, $source);
+			return $pet;
+ 		}
+	}
+	public function onPlayerQuit(PlayerQuitEvent $event) {
+		$player = $event->getPlayer();
+		$this->disablePet($player);
+	}
+	
+	public function disablePet(Player $player) {
+		if (isset(self::$pet[$player->getName()])) {
+			self::$pet[$player->getName()]->close();
+			self::$pet[$player->getName()] = null;
+		}
+	}
+	
+	public function changePet(Player $player, $newtype){
+		$type = $newtype;
+		$this->disablePet($player);
+		self::$pet[$player->getName()] = $this->createPet($player, $newtype);
+	}
+	
+	public function getPet($player) {
+		return self::$pet[$player];
+	}
+	
+	public function onJoin(PlayerJoinEvent $event){
+		$player = $event->getPlayer();
+		$data = new Config($this->getDataFolder() . "players/" . strtolower($player->getName()) . ".yml", Config::YAML);
+		if($data->exists("type")){ 
+			$type = $data->get("type");
+			$this->changePet($player, $type);
+		}
+		if($data->exists("name")){ 
+			$name = $data->get("name");
+			$this->getPet($player->getName())->setNameTag($name);
+		}
 //implementing clans!	
 		@mkdir($this->getDataFolder());
 		
