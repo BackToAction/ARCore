8764dd463b63b26e83450d83ab452edf0e79c85d
Adam Le <adamsuper169@gmail.com>
2016-08-14 10:15:36 +0700

    Base on Experience System, will add level soon

--- src/ARCore/Level/LevelSystem.php
+++ src/ARCore/Level/LevelSystem.php
@@ -1,29 +1,69 @@
 <?php
-//might rename this namespace
 namespace ARCore\Level;
-//between I'm writing this using phone so might mess up and if anyone see this message when to update this make sure to delete this comment.
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 use pocketmine\Player;
 use pocketmine\Server;
-use pocketmine\plugin\PluginBase;
-//we need 2 of this event and soon will start the project
-use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\event\player\PlayerQuitEvent;
-use pocketmine\level\Position;
-use pocketmine\level\Location;
-use pocketmine\level\Position\getLevel;
-use pocketmine\level\format\Chunk;
-use pocketmine\level\format\FullChunk;
-use pocketmine\level\Level;
-use pocketmine\level\particle\FloatingTextParticle;
 use pocketmine\math\Vector3;
-use pocketmine\math\Math;
-use pocketmine\math\AxisAlignedBB;
-use pocketmine\scheduler\PluginTask;
-use pocketmine\scheduler\CallbackTask;
-
-//P.S if you checking for exp find it on Pocketmine/Player.php
-class LevelSystem extends PluginBase implements Listener{
-
-/*TODO*/
-
+use pocketmine\utils\TextFormat;
+use pocketmine\event\Listener;
+use pocketmine\level\Position;
+use pocketmine\item\Item;
+use pocketmine\level\Level;
+use pocketmine\block\Block;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\permission\ServerOperator;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\inventory\PlayerInventory;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\utils\Config;
+use pocketmine\event\player\PlayerCommandPreprocessEvent;
+use pocketmine\event\server\ServerCommandEvent;
+use pocketmine\event\server\RemoteServerCommandEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDeathEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\block\BlockBreakEvent;
+use ARCore\ARCore;
+class LevelSystem extends PluginBase implements Listener {
+    public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        if (!file_exists($this->getDataFolder())) {
+            mkdir($this->getDataFolder(), 0744, true);
+            $this->exps = new Config($this->getDataFolder() . "exps.json", Config::JSON, array());
+        }
+        $this->exps = new Config($this->getDataFolder() . "exps.json", Config::JSON, array());
+    }
+    public function onDisable() {
+        $this->getLogger()->info(TextFormat::GREEN . "Exp function is disable!");
+    }
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
+        return false;
+    }
+    public function onJoin(PlayerJoinEvent $event) {
+        $name = $event->getPlayer()->getName();
+        if (!$this->exps->exists($name)) {
+            $this->exps->set($name, 0);
+            $this->exps->save();
+        }
+    }
+    public function onPlayerDeath(PlayerDeathEvent $event) {
+        $ev = $event->getEntity()->getLastDamageCause();
+        if ($ev instanceof EntityDamageByEntityEvent) {
+            $killer = $ev->getDamager();
+            if ($killer instanceof Player) {
+                $exp = $this->exps->get($killer->getName());
+                $exp = intval($exp) + 8; //?o?±?l
+				$killer->sendMessage("§b【RED】 Bạn nhận được 8 điểm kinh nghiệm");
+				$killer->sendMessage("§b【RED】 Số kinh nghiệm của bạn hiện tại là: ".$exp." điểm");
+				$killer->sendPopUp("§b【RED】 Bạn nhận được 8 điểm kinh nghiệm");//Popupの上にメッセージを表示
+				$killer->sendPopUp("§b【RED】 Số kinh nghiệm: ".$exp." điểm");
+                $this->exps->set($killer->getName(), $exp);
+                $this->exps->save();
+            }
+        }
+    }
 }
