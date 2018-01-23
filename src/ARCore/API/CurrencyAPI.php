e74fd30c5e2dee82bb3c66719076a48c8da5286d
HyGlobalHD <GamerXzavier@gmail.com>
2018-01-23 13:04:11 +0800

    small update
    add small API

---
+++ src/ARCore/API/CurrencyAPI.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace ARCore\API;
+
+use pocketmine\utils\Config;
+use pocketmine\utils\Utils;
+use ARCore\ARCore;
+use ARCore\API\DatabaseAPI;
+
+class CurrencyAPI {
+
+    // Using My Simplified API From BTACore-PMMP
+    // modified a bit for ARCore Comparebility
+
+    static public $instance;
+
+	public function __construct(ARCore $plugin){
+        $this->plugin = $plugin;
+        $this->database = new DatabaseAPI::getInstance();
+    }
+
+    static public function getInstance(){// allow to get CurrencyAPI Instance.
+        if (is_null(self::$instance)) {// if there is no instance
+             self::$instance = new self();// make new instance
+        }
+        return self::$instance; // else (if this have instance) return instance
+    }
+
+    public function getCoins($user){ // simple and friendly use API... Ez To Learn
+        $i = $this->database->getPlayerDatabase($user);
+        $result = $i->get("Coins");
+        return $result;
+        $i->save();
+    }
+
+    public function setCoins($user, $coins){
+        $i = $this->database->getPlayerDatabase($user);
+        if($coins >= 0){
+            $result = $i->set("Coins", $coins);
+            return $result;
+            $i->save();
+        }
+    }
+
+    public function addCoins($user, $coins){
+        $i = $this->database->getPlayerDatabase($user);
+        if($coins >= 0){// the numbers is not negative, must be positive
+            $lastest = $i->get("Coins");
+            $i->set("Coins", $lastest + $coins);
+            $i->save();
+        }
+    }
+
+    public function reduceCoins($user, $coins){
+        $i = $this->database->getPlayerDatabase($user);
+        if($coins >= 0){
+            $lastest = $i->get("Coins");
+            if($coins <= $lastest){
+                $i->set("Coins", $lastest - $coins);
+                $i->save();
+            }
+        }
+    }
+
+
+
+
+}
+
+?>
---
+++ src/ARCore/API/DatabaseAPI.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace ARCore\API;
+
+use pocketmine\utils\Config;
+use pocketmine\utils\Utils;
+
+class DatabaseAPI {
+
+    static private $instance;
+
+    public function __construct(ARCore $plugin){
+        $this->plugin = $plugin;
+        $this->conf = new Config($this->getDataFolder() . "config.yml");
+    }
+    
+    static private function getInstance(){ // make it private function
+        if (is_null(self::$instance)) {
+                self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function getPlayerDatabase($user){
+        $ign = strtolower($user);
+        $hasher = hash(sha256, $ign);// hmm?? just bored.
+        if($this->conf->get("Enable%FGC") == true){// Enable FGC To Use It!!
+            $fgc = $this->conf->get("FGC.Feature");
+            $result = new Config($this->plugin->getDataFolder() . "\\data\\players\\" . $hasher . ".yml", CONFIG::YAML, array(
+                "Username" => $ign,
+                "Coins" => 0,
+                "Level" => 1,
+                "Exp" => 0,
+                "Class" => 0,
+                "Health" => 10,
+                "Mana" => 10,
+                "Stats" => [
+                    "str" => 5, "int" => 5, "dex" => 5, "luck" => 5,
+                ],
+                "Skills" => [
+                    "Normal Attack" => "Just A Normal Attack",
+                ],
+                "Title" => [
+                    "New Lancer" => "You Still Confuse With The Server As You Still New.",
+                ],
+                "Description" => "Tell A Bit About Yourself",
+                "Likes" => 0,
+                "Achievements" => 0,
+                "WarnPoints" => 0,
+                "$fgc" => "",
+                "Friends" => [],
+            ));
+            return $result;
+        }else{
+            $result = new Config($this->plugin->getDataFolder() . "\\data\\players\\" . $hasher . ".yml", CONFIG::YAML, array(
+                "Username" => $ign,
+                "Coins" => 0,
+                "Level" => 1,
+                "Exp" => 0,
+                "Class" => 0,
+                "Health" => 10,
+                "Mana" => 10,
+                "Stats" => [
+                    "str" => 5, "int" => 5, "dex" => 5, "luck" => 5,
+                ],
+                "Skills" => [
+                    "Normal Attack" => "Just A Normal Attack",
+                ],
+                "Title" => [
+                    "New Lancer" => "You Still Confuse With The Server As You Still New.",
+                ],
+                "Description" => "Tell A Bit About Yourself",
+                "Likes" => 0,
+                "Achievements" => 0,
+                "WarnPoints" => 0,
+                "Friends" => [],
+            ));
+            return $result;
+        }
+    }
+
+    public function getPlayerParty($party_name){
+        // todo
+        // put it in \\data\\partys\\
+        // party is a small branch of FGC which doesn't effect FGC
+        // it enable player to form a party of 8 maximum,
+        // for: enable to enter a specific instance(eg: dungeon) and clear it together
+        // todo 
+    }
+
+    public function getEntityDatabase($entity){
+        //... Todo
+        // Design Note: For Making A Custom Mob.
+    }
+
+
+}
+
+?>
